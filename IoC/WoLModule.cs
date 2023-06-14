90b05592675894c32e0654a567d8677539370ea6
zhukov <zhukov@tvema.ru>
2023-06-14 15:56:19 +0300

    wake on lan implementation

---
+++ IoC/WoLModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject;
+using Ninject.Modules;
+using TeleWoL.Settings;
+using TeleWoL.WakeOnLan;
+
+namespace TeleWoL.IoC;
+
+internal class WoLModule : NinjectModule
+{
+    public override void Load()
+    {
+        var settings = Kernel.Get<GlobalSettings>();
+        var sender = new MultipleNicWoLSender(); // NICs can be selected here
+        Bind<IWoLSender>().ToMethod(_ => sender).InSingletonScope();
+    }
+}
